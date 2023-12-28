61fa99eb4966a88678a82821fd13b17df7a93e5e
Angel Palacios Mirafuentes <al221910938@gmail.com>
2023-12-28 14:11:26 -0600

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dependencies for a angular project
+# Install nodejs npm nginx
+sudo apt-get install -y nodejs npm nginx
+
+# Install angular cli
+sudo npm install -g @angular/cli
+
+
---
+++ scripts/start_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Configure nginx to serve the angular app on port 80 
+
+echo "Hola mundo"
+echo "Aqui se configura nginx para servir la app angular en el puerto 80"
