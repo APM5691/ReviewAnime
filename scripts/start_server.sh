aad70efbc6ec23dbea39d2881cc1f77a77f72ebb
Angel Palacios Mirafuentes <al221910938@gmail.com>
2023-12-28 15:27:08 -0600

    Update nginx configuration and compile Angular app

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Configure nginx to serve the angular app on port 80 
+# compile the angular app
 
-echo "Hola mundo"
-echo "Aqui se configura nginx para servir la app angular en el puerto 80"
+cd /var/www/html/deploy
+sudo ng build --prod --base-href /deploy/
+sudo cp -r /var/www/html/deploy/dist/deploy/* /var/www/html/
+sudo rm -rf /var/www/html/deploy
+sudo systemctl restart nginx
