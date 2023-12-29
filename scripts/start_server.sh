3a2a1af9c9b6f5512a7503ebf444ac38a9a1687e
Angel Palacios Mirafuentes <al221910938@gmail.com>
2023-12-28 19:48:34 -0600

    Update start_server.sh script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,8 +3,8 @@ cd /var/www/html/files
 npm install
 ng build
 cp -r dist/markdown/* /var/www/html/build
-rm -rf /var/www/html/files/*
-rm -rf /var/www/html/files/.*
+# rm -rf /var/www/html/files/*
+# rm -rf /var/www/html/files/.*
 
 # restart nginx
 service nginx restart
