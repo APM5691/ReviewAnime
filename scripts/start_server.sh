ae3af04e17aea21f0c06a1975f8eb7f624409722
Angel Palacios Mirafuentes <al221910938@gmail.com>
2023-12-28 19:34:50 -0600

    Update start_server.sh script to clean up files after build

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-# Configure nginx to serve the angular app on port 80 
-# compile the angular app
-
 cd /var/www/html/files
 npm install
 ng build --prod 
 cp -r dist/* /var/www/html/build
+rm -rf /var/www/html/files/*
 
 # restart nginx
 service nginx restart
