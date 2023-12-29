7eea100f0a7841e35c0e4a0f1d7df156bb5eb942
Angel Palacios Mirafuentes <al221910938@gmail.com>
2023-12-28 19:30:43 -0600

    Update appspec.yml and start_server.sh

--- scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install dependencies for a angular project
-# Install nodejs npm
-sudo apt install -y nodejs npm
-
-# Install angular cli
-sudo npm install -g @angular/cli
-
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,9 +3,10 @@
 # Configure nginx to serve the angular app on port 80 
 # compile the angular app
 
-cd /var/www/html/deploy
+cd /var/www/html/files
 npm install
-sudo ng build --prod --base-href /deploy/
-sudo cp -r /var/www/html/deploy/dist/deploy/* /var/www/html/
-sudo rm -rf /var/www/html/deploy
-sudo systemctl restart nginx
+ng build --prod 
+cp -r dist/* /var/www/html/build
+
+# restart nginx
+service nginx restart
