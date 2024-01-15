142510b3a3d4dd7927b6294a07e9ddce48313d78
Angel Palacios Mirafuentes <al221910938@gmail.com>
2024-01-14 19:55:25 -0600

    Update timeout for start_server.sh script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html/files
 sudo npm install
-sudo ng build
+sudo npm run build
 sudo cp -r dist/markdown/* /var/www/html/build
 sudo rm -rf /var/www/html/files/*
 sudo rm -rf /var/www/html/files/.*
